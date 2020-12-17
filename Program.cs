805c5d9571ef5a55cd1abde8c586197542eb9af4
Jocelynn Hartwig <jhartwig@apterainc.com>
2020-12-17 11:36:58 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+
+namespace mars_classifier_console_app
+{
+    class Program
+    {
+        public static string subscriptionKey = "41fe13457d774d4299f7b272079128b7";
+        public static string endpoint = "https://mars-image-classifier.cognitiveservices.azure.com/";
+
+        // URL image used for analyzing an image (image of puppy)
+        private const string ANALYZE_URL_IMAGE = "https://images-na.ssl-images-amazon.com/images/I/71LkML0SXML.png";
+
+        private const string READ_TEXT_URL_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/0/0d/1%2B1%3D2.png";
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+            new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+            { Endpoint = endpoint };
+            return client;
+        }
+
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
+
+            // Creating a list that defines the features to be extracted from the image. 
+
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+                VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+                VisualFeatureTypes.Tags, VisualFeatureTypes.Adult,
+                VisualFeatureTypes.Color, VisualFeatureTypes.Brands,
+                VisualFeatureTypes.Objects
+            };
+            ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, features);
+            Console.WriteLine("Objects:");
+            foreach (var obj in results.Objects)
+            {
+                Console.WriteLine($"{obj.ObjectProperty} with confidence {obj.Confidence} at location {obj.Rectangle.X}, " +
+                  $"{obj.Rectangle.X + obj.Rectangle.W}, {obj.Rectangle.Y}, {obj.Rectangle.Y + obj.Rectangle.H}");
+            }
+            Console.WriteLine();
+        }
+
+        /*
+ * READ FILE - URL 
+ * Extracts text. 
+ */
+        public static async Task ReadFileUrl(ComputerVisionClient client, string urlFile)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("READ FILE FROM URL");
+            Console.WriteLine();
+
+            // Read text from URL
+            var textHeaders = await client.ReadAsync(urlFile, language: "en");
+            // After the request, get the operation location (operation ID)
+            string operationLocation = textHeaders.OperationLocation;
+            Thread.Sleep(2000);
+
+            // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+            // Extract the text
+            ReadOperationResult results;
+            Console.WriteLine($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+            Console.WriteLine();
+            do
+            {
+                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running ||
+                results.Status == OperationStatusCodes.NotStarted));
+
+            // Display the found text.
+            Console.WriteLine();
+            var textUrlFileResults = results.AnalyzeResult.ReadResults;
+            foreach (ReadResult page in textUrlFileResults)
+            {
+                foreach (Line line in page.Lines)
+                {
+                    Console.WriteLine(line.Text);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            ComputerVisionClient client = Authenticate(Program.endpoint, subscriptionKey);
+            // Analyze an image to get features and other properties.
+            AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
+
+            // Extract text (OCR) from a URL image using the Read API
+            ReadFileUrl(client, READ_TEXT_URL_IMAGE).Wait();
+
+
+        }
+    }
+}
